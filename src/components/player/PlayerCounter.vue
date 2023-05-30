9e01db87bbd3b82957d9e317189c7ec98658d87f
marco <marco@wiz-team.com>
2023-05-30 15:04:58 +0200

    player counter component

---
+++ src/components/player/PlayerCounter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="player">
+        <h3 class="player-name">{{ name }}</h3>
+        <div class="score">
+        <h3>{{ score }}</h3>
+        <div class="score-cmd">
+            <span>-</span>
+            <span>+</span>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        name: String,
+        score: Number
+    }
+}
+</script>
