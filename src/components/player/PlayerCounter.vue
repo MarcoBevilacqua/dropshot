7fe34e728135f70cce82720a783325e7446c267a
marco <marco@wiz-team.com>
2023-05-31 13:55:48 +0200

    working ugly buttons

--- src/components/player/PlayerCounter.vue
+++ src/components/player/PlayerCounter.vue
@@ -1,22 +1,38 @@
+<script setup>
+import { onMounted, reactive } from 'vue'
+
+// reactive state
+const state = reactive({score: 0})
+
+// functions that mutate state and trigger updates
+function increment() {
+  state.score++
+}
+
+function decrement() {
+  state.score--
+}
+
+defineProps({
+    name: String,
+    score: Number
+})
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${state.score}.`)
+})
+</script>
+
 <template>
     <div class="player">
         <h3 class="player-name">{{ name }}</h3>
         <div class="score">
-        <h3>{{ score }}</h3>
+        <h3>{{ state.score }}</h3>
         <div class="score-cmd">
-            <span>-</span>
-            <span>+</span>
+            <button @click="decrement">-</button>
+            <button @click="increment">+</button>
         </div>
         </div>
     </div>
 </template>
-
-<script>
-
-export default {
-    props: {
-        name: String,
-        score: Number
-    }
-}
-</script>
